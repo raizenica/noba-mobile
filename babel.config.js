ea015b482786e5e3bf738d7740a686f48ed7316b
raizenica <strikerke@gmail.com>
2026-04-03 15:12:21 +0200

    chore: add copyright headers to all source files
    Copyright (c) 2024-2026 Kevin Van Nieuwenhove. All rights reserved.
    Licensed under Apache 2.0.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,5 @@
+// Copyright (c) 2024-2026 Kevin Van Nieuwenhove. All rights reserved.
+// NOBA Command Center — Licensed under Apache 2.0.
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
 };
